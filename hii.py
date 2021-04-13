c191817bc090acd0f6035547a59986b0624915e8
Rahul-learner <66640990+Rahul-learner@users.noreply.github.com>
2021-04-13 13:17:31 +0530

    Add files via upload

---
+++ hii.py
@@ -0,0 +1,24 @@
+import cv2
+import time
+import PoseModule as pm
+
+cap = cv2.VideoCapture('http://192.168.0.101:4747/video')
+pTime = 0
+detector = pm.poseDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList = detector.findPosition(img,draw=False)
+    if len(lmList) != 0:
+        print(lmList[14])
+        cv2.circle(img, (lmList[14][1], lmList[14][2]), 10, (0, 0, 255), cv2.FILLED)
+
+    cTime = time.time()
+    fps = 2.5 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
